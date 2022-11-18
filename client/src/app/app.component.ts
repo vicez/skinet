c60e0a46681fd9d860c797876f38011602174e1c
Vicez <akinseindevictor@gmail.com>
2022-11-18 01:17:11 +0100

    end of section 8

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { IPagination } from './models/pagination';
+import { IProduct } from './models/product';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Skinet';
+  products: IProduct[] = [];
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit(): void {
+    // let param: HttpParams = new HttpParams().set("pageSize", 50)
+    this.http.get("https://localhost:5001/api/products?pageSize=50").subscribe((res: IPagination) => {
+      this.products = res.data;
+    }, error => {
+      console.log(error)
+    });
+  }
+
+
+
 }
